69533495ba7a4f90fe5075fccf3a4db9672feae8
Makis Otman <makisotman@gmail.com>
2014-01-25 14:27:07 +0000

    started fresh, test checking default capacity at airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,9 @@
 class Airport
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 10
 
+  def initialize(options = {})
+    self.capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe Airport do
+let(:airport) { Airport.new }
 
+context 'ability to set airport capacity' do
 
+  it 'should have a default capacity' do
+    expect(airport.capacity).to eq(10)
+  end
+
+end
 
 end
