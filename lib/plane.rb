392d04c05476f7829f6608cab7194eb96e44fa8a
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2014-01-27 11:08:17 +0000

    feedback

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,5 @@
+# it would have been nice to add some behaviour to the plane,
+# e.g. it's flying by default but it's landed and not flying after you land it, etc.
 class Plane
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Airport do
-let(:airport) { Airport.new }
-let(:plane) { Plane.new }
+  let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
 
   context "airport capacity" do
-
+    # two good tests
     it "should have a default capacity" do
       expect(airport.capacity).to eq(10)
     end
@@ -18,29 +18,38 @@ let(:plane) { Plane.new }
 
   context "landing and taking off" do
 
+    # Fails out of the box
     it "plane can land" do
       airport.land(plane)
       expect(airport.planes).to eq([plane])
+      # a better option: expect(airport.plane_count).to eq(1)
     end
 
+    # fails out of the box
     it "plane can take off" do
       airport.land(plane)
       airport.take_off(plane)
       expect(airport.planes).to eq([])
+      # it would be better to do airport.empty?
     end
   end
 
   context "traffic control" do
     it "plane cannot land if airport is full" do
+      # a better way would be to land 10 planes
+      # and then check that the eleventh raises an error
+      # because this test would pass even if the airport
+      # throwed an exception every single time you landed a plane
       expect { 11.times {airport.land(plane)} }.to raise_error
     end
   end
 
   context "weather conditions" do
-
+    # This test fails out of the box.
+    # Tests should always pass when you check out the project
     it "plane cannot land if weather is stormy" do
       airport.stub(:stormy_weather?) { true }
-      expect(airport.land(plane)).to raise_error
+      expect{airport.land(plane)}.to raise_error
     end
   end
 end
